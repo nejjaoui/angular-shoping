fe8544a7843bf6aa66d843cc32f84a68f90efcaa
Abdelilah <abdelilah@x.home>
2019-12-27 19:10:33 +0100

    Setting up home components and testing add function

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Good } from 'src/app/interfaces/good.interface';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+
+  goods: Good [] = [
+    {name:'apple',price:5,photoUrl:'assets/f5.jpeg'},
+    {name:'banana',price:10,photoUrl:'assets/f2.jpeg'},
+    {name:'orange',price:6,photoUrl:'assets/f3.jpeg'},
+    {name:'avocado',price:13,photoUrl:'assets/f1.jpeg'}
+  ]
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addToCart(index){
+    console.log('added',this.goods[index])
   }
 
 }
---
+++ src/app/interfaces/good.interface.ts
@@ -0,0 +1,5 @@
+export interface Good {
+    name? : string,
+    price? : number,
+    photoUrl : string
+}
